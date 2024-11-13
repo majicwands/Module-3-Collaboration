52969e153b70b4bc81248a05a161f9f953222a8f
william wands <wmwands@outlook.com>
2024-11-13 08:23:05 -0500

    superclass

---
+++ Mo3-CaseStudy-superClass.py
@@ -0,0 +1,25 @@
+#A super class called Vehicle, which contains an attribute for vehicle type, such as car, truck, plane, boat, or a broomstick.
+#A class called Automobile which will inherit the attributes from Vehicle and also contain the following attributes:
+#year
+#make
+#model
+#doors (2 or 4)
+#roof (solid or sun roof).
+
+
+#super()
+
+
+class automobile:
+    def __init__(self, year, make, model, doors, roof):
+        self.year = year 
+        self.make = make
+        self.model = model
+        self.doors = doors
+        self.roof = roof
+
+
+class truck(automobile):
+    def __init__(self, year, make, model, doors, roof):
+        super().__init__(any, any, any, any ->)
+        
